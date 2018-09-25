e7a26398fb67095018f7273c122c996ab730bf8b
Huyd <huyidada@gmail.com>
2018-09-25 10:49:54 +0800

    First idea

--- daily/First.java
+++ daily/First.java
@@ -1,7 +1,8 @@
 /**
  * Author: huyd
  * Date: 2018/9/25 10:42
- * Description:题目：求1+2+…+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字以及条件判断语句（A?B:C）。
+ * Description: 题目：求1+2+…+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字以及条件判断语句（A?B:C）。
+ * Idea: 利用短路求值的方式
  */
 
 class First{
