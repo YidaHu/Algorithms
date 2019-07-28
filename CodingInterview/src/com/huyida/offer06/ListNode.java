2bb1a519e1c26bd9a4656e9ca7706a366d7dc887
huyida <huyidada@163.com>
2019-07-28 23:03:32 +0800

    6.从尾到头打印链表 使用栈的方法

---
+++ CodingInterview/src/com/huyida/offer06/ListNode.java
@@ -0,0 +1,11 @@
+package com.huyida.offer06;
+
+/**
+ * @program: CodingInterview
+ * @description: 链表
+ * @author: huyida
+ * @create: 2019-07-28 22:32
+ **/
+
+public class ListNode {
+}
---
+++ CodingInterview/src/com/huyida/offer06/PrintListFromTailToHead.java
@@ -0,0 +1,11 @@
+package com.huyida.offer06;
+
+/**
+ * @program: CodingInterview
+ * @description: 从尾到头打印链表
+ * @author: huyida
+ * @create: 2019-07-28 22:28
+ **/
+
+public class PrintListFromTailToHead {
+}
